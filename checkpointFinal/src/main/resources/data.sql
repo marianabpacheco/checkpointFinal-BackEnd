197012f44fd45a91709bd8dd54fe99dc63953e0d
Mariana Pacheco <marianabtpacheco@gmail.com>
2021-12-05 12:25:05 -0300

    adição da configuração do cors e inserção de dados no banco

---
+++ checkpointFinal/src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO tb_category (name) VALUES ('celulares'),('tvs'),('notebooks'),('informática');
+
+INSERT INTO tb_product (title,price,description,image,category_id) VALUES ('Smartphone Samsung Galaxy S20 Fe',2294.99,'Smartphone Samsung Galaxy S20 Fe 128GB 4G Wi-Fi Tela 6.5″ Dual Chip 6GB RAM Câmera Tripla + Selfie 32MP - Cor: Cloud Mint','https://a-static.mlcdn.com.br/618x463/smartphone-samsung-galaxy-s20-fe-128gb-cloud-mint-4g-6gb-ram-tela-65-cam-tripla-selfie-32mp/magazineluiza/155630400/5f2870ff3ce9ab58274fe9df33ec846b.jpg',1),
+('Smartphone Moto G60', 1869.10,'Smartphone Moto G60 128GB 4G Wi-Fi Tela 6.8″ Dual Chip 6GB RAM Câmera Tripla + Selfie 32MP - Cor: Azul','https://images-americanas.b2w.io/produtos/01/00/img/3177003/9/3177003910_1SZ.jpg',1),
+('iPhone 12 Pro',9110.90,'iPhone 12 Pro Apple (512GB) Grafite tela 6,1″ Câmera tripla 12MP iOS','https://images-americanas.b2w.io/produtos/01/00/img/2290965/8/2290965854_1SZ.jpg',1),
+('Smartphone Redmi Note 9',2299.99,'Smartphone Redmi Note 9, 128GB, 48MP, Tela 6.53″ - Cor: Preto','https://images-americanas.b2w.io/produtos/3042339096/imagens/smartphone-redmi-note-9-128gb-48mp-tela-6-53-preto/3042339096_1_xlarge.jpg',1),
+('Smartphone LG K62+',1149.00,'Smartphone LG K62+ 128GB 4G Wi-Fi Tela 6.6″ Dual Chip 4GB RAM Câmera Quádrupla + Selfie 28MP - Azul','https://images-americanas.b2w.io/produtos/01/00/img/2324593/8/2324593834_1SZ.jpg',1),
+('Smartphone Realme 7',2199.00, 'Smartphone Realme 7 Pro Mirror 128GB 4G Wi-Fi Tela 6.4″ Dual Chip 8GB RAM Câmera Tripla + Selfie 32MP - Cor: Silver','https://images-americanas.b2w.io/produtos/01/00/img/3191812/8/3191812879_1SZ.jpg',1);
+
+INSERT INTO tb_product (title,price,description,image,category_id) VALUES ('Smart Tv Philco', 1100.99,'Smart Tv Philco 24″ Resolução Em Hd Fast Smart Com Processador Quadcore - Ptv24g50sn Led', 'https://images-americanas.b2w.io/produtos/01/00/img/1803456/4/1803456474_1SZ.jpg',2),
+('Smart TV Android LED 32″ Semp 32S5300',1400.99,'Smart TV Android LED 32″ Semp 32S5300 Bluetooth 2 HDMI 1 USB Controle Remoto com Comando de Voz e Google Assistant','https://images-americanas.b2w.io/produtos/01/00/img/134511/8/134511869_1SZ.jpg',2),
+('Smart TV LG 43″ Full HD',2299.99,'Smart TV LG 43″ Full HD 43LM6370 Wi-Fi Bluetooth HDR Thinqai Compatível Com Inteligência Artificial','https://images-americanas.b2w.io/produtos/01/00/img/3397039/7/3397039711_1SZ.jpg',2),
+('Smart TV LG 50″ 4K NanoCell',3499.90,'Smart TV LG 50″ 4K NanoCell 50NANO75 3x HDMI 2.0 Inteligência Artificial Thinqai Smart Magic Google Alexa','https://images-americanas.b2w.io/produtos/01/00/img/3397040/7/3397040747_1SZ.jpg',2),
+('Smart TV Tela QLED 55″ Samsung', 4200.89,'Smart TV Tela QLED 55″ Samsung 55q60a 4K Conversor Digital Wi-Fi integrado 2 USB 3 HDMI','https://images-americanas.b2w.io/produtos/01/00/img/3060874/9/3060874914_1SZ.jpg',2),
+('Smart TV LG OLED 65″', 10100.99,'Smart TV LG OLED 65″ 4K OLED65C1 120hz G-Sync Freesync 4x HDMI 2.1 Inteligência Artificial Thinq Google Alexa','https://images-americanas.b2w.io/produtos/01/00/img/3280462/7/3280462719_1SZ.jpg',2);
+
+INSERT INTO tb_product (title,price,description,image,category_id) VALUES ('Notebook Samsung Book',3569.99,'Notebook Samsung Book NP550XDA-KF2BR Intel Core i5-1135G7 8GB 256GB SSD (Intel Iris Xe) Tela 15.6″ Windows 10 FHD - Cor: Cinza Chumbo','https://images-americanas.b2w.io/produtos/01/00/img/3081358/9/3081358919_1SZ.jpg',3),
+('Notebook Lenovo Ultrafino Ideapad',3229.99,'Notebook Lenovo Ultrafino Ideapad 3i Intel Core i5-10210U 8GB 256GB SSD W10 15.6″ - Cor: Prata','https://images-americanas.b2w.io/produtos/01/00/img/3171318/1/3171318144_1SZ.jpg',3),
+('Notebook Acer Aspire 5',7789.10,'Notebook Acer Aspire 5 AN515-55-705U Intel Core i7-10750H 8GB (GeForce GTX 1660 TI 6GB) 512GB W10 15.6″ - Cor: Preto','https://images-americanas.b2w.io/produtos/01/00/img/3191812/1/3191812182_1SZ.jpg',3),
+('Notebook Dell Inspiron',3610.75,'Notebook Dell Inspiron I15-3501-a46p 15.6″ Hd 10ª Geração Intel Core I5 8gb 256gb Ssd Windows - Cor: Preto','https://images-americanas.b2w.io/produtos/01/00/img/3716186/4/3716186496_3SZ.jpg',3),
+('MacBook Air 13″',8300,'MacBook Air 13″ Apple M1 (8GB 256GB) - Cor: Prata','https://images-americanas.b2w.io/produtos/01/00/img/2520061/5/2520061536_1SZ.jpg',3),
+('Notebook Asus Vivobook',2300,'Notebook Asus Vivobook X543UA-GQ3430 Intel Core i3-7020U 4G 256GB SSD Tela 15,6″ Endless Os - Cor: Cinza Escuro','https://images-americanas.b2w.io/produtos/01/00/img/4209827/0/4209827068_1SZ.jpg',3);
+
+
+INSERT INTO tb_product (title,price,description,image,category_id) VALUES ('Mouse Sem Fio Logitech M720 Triathlon',239.55,'Mouse Sem Fio Logitech M720 Triathlon com Conexão USB Unifying ou Bluetooth com Easy-Switch Para Até 3 Dispositivos e Pilha Inclusa','https://images-americanas.b2w.io/produtos/01/00/img/133670/1/133670112_1SZ.jpg',4),
+('Teclado Com Fio/bluetooth Motospeed',470,'Teclado Com Fio/bluetooth Motospeed Ck62 Rosa Switch Azul Rgb','https://images-americanas.b2w.io/produtos/01/00/img/3223550/4/3223550430_1SZ.jpg',4),
+('HD Externo Seagate',299.89,'HD Externo Seagate 1TB Expansion Preto','https://images-americanas.b2w.io/produtos/01/00/img/124488/4/124488482_1SZ.jpg',4),
+('Roteador Dual Band TP-Link',249.90,'Roteador Dual Band TP-Link AC1350 Archer C60 450Mbps e 867Mbps 5 Portas 3 Antenas','https://images-americanas.b2w.io/produtos/01/00/img/134544/2/134544273_1SZ.jpg',4),
+('Monitor LED 24″ Gamer LG',1439.99,'Monitor LED 24″ Gamer LG 24GL600F 1920x1080 1ms 144hz Full HD Freesync','https://images-americanas.b2w.io/produtos/01/00/img/134472/3/134472396_1SZ.jpg',4),
+('Impressora Multifuncional Epson Ecotank L3250',1200.99,'Multifuncional Epson Ecotank L3250 Wi-Fi - Tanque de Tinta','https://images-americanas.b2w.io/produtos/01/00/img/3831314/1/3831314123_1SZ.jpg',4);
